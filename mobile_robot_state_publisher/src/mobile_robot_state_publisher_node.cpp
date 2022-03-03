fdac0d7981c378ac1fd1af19b6fdd2e5c9d91d53
Oscar de Groot <o.m.degroot@tudelft.nl>
2022-03-03 10:28:35 +0100

    last state.

--- mobile_robot_state_publisher/src/mobile_robot_state_publisher_node.cpp
+++ mobile_robot_state_publisher/src/mobile_robot_state_publisher_node.cpp
@@ -40,32 +40,32 @@ geometry_msgs::Pose pos;
 geometry_msgs::Twist vel;
 nav_msgs::Odometry odom_msg;
 
+void VelocityCallBack(const gazebo_msgs::LinkStates &msg)
+{
+	int index = 0;
+	std::string str2("base_link");
+	for (int i = 0; i < sizeof(msg.name); i++)
+	{
 
-void VelocityCallBack(const gazebo_msgs::LinkStates& msg){
-	int index=0;
-	std::string str2 ("base_link");
-	for(int i =0; i< sizeof(msg.name);i++){
-
-		if(msg.name[i].find(str2)!=std::string::npos)
+		if (msg.name[i].find(str2) != std::string::npos)
 			break;
-		index+=1;
+		index += 1;
 	}
 	//ROS_INFO_STREAM("fOUND IN: " << index);
-    pos = msg.pose[index];
-    odom_msg.twist.twist = msg.twist[index];
-    odom_msg.pose.pose = msg.pose[index];
-    odom_msg.child_frame_id="base_link";
-    odom_msg.header.frame_id="odom";
-    odom_msg.header.stamp=ros::Time::now();
-/* This is done because the ukf does not match the position in Gazebo */
+	pos = msg.pose[index];
+	odom_msg.twist.twist = msg.twist[index];
+	odom_msg.pose.pose = msg.pose[index];
+	odom_msg.child_frame_id = "base_link";
+	odom_msg.header.frame_id = "odom";
+	odom_msg.header.stamp = ros::Time::now();
+	/* This is done because the ukf does not match the position in Gazebo */
 	static tf::TransformBroadcaster br;
 	tf::Transform transform;
-	transform.setOrigin( tf::Vector3(pos.position.x,pos.position.y,pos.position.z) );
-	tf::Quaternion q(pos.orientation.x,pos.orientation.y,pos.orientation.z,pos.orientation.w);
+	transform.setOrigin(tf::Vector3(pos.position.x, pos.position.y, pos.position.z));
+	tf::Quaternion q(pos.orientation.x, pos.orientation.y, pos.orientation.z, pos.orientation.w);
 
 	transform.setRotation(q);
 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_link"));
-
 }
 
 int main(int argc, char **argv)
@@ -74,43 +74,42 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	ros::Subscriber robot_state_sub_;
 
-
 	double node_rate;
-	if (!n.getParam(ros::this_node::getName()+"/rate", node_rate))
+	if (!n.getParam(ros::this_node::getName() + "/rate", node_rate))
 	{
-		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName()+"/rate not set");
+		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName() + "/rate not set");
 		return 0;
 	}
 
 	string root_frame;
-	if (!n.getParam(ros::this_node::getName()+"/root_frame", root_frame))
+	if (!n.getParam(ros::this_node::getName() + "/root_frame", root_frame))
 	{
-		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName()+"/root_frame not set");
+		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName() + "/root_frame not set");
 		return 0;
 	}
 
 	string base_frame;
-	if (!n.getParam(ros::this_node::getName()+"/base_frame", base_frame))
+	if (!n.getParam(ros::this_node::getName() + "/base_frame", base_frame))
 	{
-		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName()+"/base_frame not set");
+		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName() + "/base_frame not set");
 		return 0;
 	}
 
 	string robot_state_topic;
-	if (!n.getParam(ros::this_node::getName()+"/robot_state_topic", robot_state_topic))
+	if (!n.getParam(ros::this_node::getName() + "/robot_state_topic", robot_state_topic))
 	{
-		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName()+"/robot_state_topic not set");
+		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName() + "/robot_state_topic not set");
 		return 0;
 	}
 
 	string vel_state_topic;
-	if (!n.getParam(ros::this_node::getName()+"/vel_state_topic", vel_state_topic))
+	if (!n.getParam(ros::this_node::getName() + "/vel_state_topic", vel_state_topic))
 	{
-		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName()+"/vel_state_topic not set");
+		ROS_ERROR_STREAM("mobile_robot_state_publisher_node Parameter " << ros::this_node::getName() + "/vel_state_topic not set");
 		return 0;
 	}
 
-    robot_state_sub_ = n.subscribe(vel_state_topic, 1, VelocityCallBack);
+	robot_state_sub_ = n.subscribe(vel_state_topic, 1, VelocityCallBack);
 
 	ros::Publisher state_pub_ =
 		n.advertise<geometry_msgs::PoseStamped>(robot_state_topic, 10);
@@ -119,7 +118,7 @@ int main(int argc, char **argv)
 	ros::ServiceClient link_state_client_ = n.serviceClient<gazebo_msgs::SetLinkState>("/gazebo/set_link_state");
 
 	gazebo_msgs::SetLinkState link;
-	link.request.link_state.link_name="base_link";
+	link.request.link_state.link_name = "base_link";
 
 	tf2_ros::Buffer tfBuffer;
 	tf2_ros::TransformListener tfListener(tfBuffer);
@@ -127,13 +126,14 @@ int main(int argc, char **argv)
 	ros::Rate rate(node_rate);
 	geometry_msgs::PoseStamped pose_msg;
 
-    ros::Publisher odom_pub_ = n.advertise<nav_msgs::Odometry>("/odometry/filtered", 10);
+	ros::Publisher odom_pub_ = n.advertise<nav_msgs::Odometry>("/odometry/filtered", 10);
 
 	//Intermidiate variables
 	double ysqr, t3, t4;
 	geometry_msgs::TransformStamped transformStamped;
 	ros::Rate r(20.0);
-	while (n.ok()){
+	while (n.ok())
+	{
 		/*
 		try{
 			transformStamped = tfBuffer.lookupTransform(root_frame, base_frame,
@@ -147,18 +147,18 @@ int main(int argc, char **argv)
 		//CONVERT FROM QUATERNION TO JOINT ANGLE ROTATION
 		*/
 		pose_msg.header.frame_id = "odom";
-		pose_msg.header.stamp = ros::Time::now();;
+		pose_msg.header.stamp = ros::Time::now();
+		;
 
 		ysqr = odom_msg.pose.pose.orientation.y * odom_msg.pose.pose.orientation.y;
-		t3 = +2.0 * (odom_msg.pose.pose.orientation.w * odom_msg.pose.pose.orientation.z
-					 + odom_msg.pose.pose.orientation.x * odom_msg.pose.pose.orientation.y);
+		t3 = +2.0 * (odom_msg.pose.pose.orientation.w * odom_msg.pose.pose.orientation.z + odom_msg.pose.pose.orientation.x * odom_msg.pose.pose.orientation.y);
 		t4 = +1.0 - 2.0 * (ysqr + odom_msg.pose.pose.orientation.z * odom_msg.pose.pose.orientation.z);
 
 		pose_msg.pose.orientation.z = atan2(t3, t4);
 		pose_msg.pose.position.x = odom_msg.pose.pose.position.x;
 		pose_msg.pose.position.y = odom_msg.pose.pose.position.y;
 
-		pose_msg.pose.position.z = std::sqrt(std::pow(odom_msg.twist.twist.linear.x,2)+std::pow(odom_msg.twist.twist.linear.y,2));
+		pose_msg.pose.position.z = std::sqrt(std::pow(odom_msg.twist.twist.linear.x, 2) + std::pow(odom_msg.twist.twist.linear.y, 2));
 
 		state_pub_.publish(pose_msg);
 		/*
@@ -172,7 +172,7 @@ int main(int argc, char **argv)
 		link_state_client_.call(link);
 		 */
 		//link_state_pub_.publish(link);
-        odom_pub_.publish(odom_msg);
+		odom_pub_.publish(odom_msg);
 		ros::spinOnce();
 		r.sleep();
 	}
